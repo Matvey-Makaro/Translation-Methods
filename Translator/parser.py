0cd42db4708e21810817135d0258f2142227f877
Matvey-Makaro <matvey.makaro@gmail.com>
2023-03-26 16:21:25 +0300

    Add UsingBeforeDeclarationError, DoubleDeclarationError, _parse_declare_identifier(), _parse_using_identifier(), _parse_to_string().

--- Translator/parser.py
+++ Translator/parser.py
@@ -17,6 +17,16 @@ class ExpectedError(ParserError):
         super().__init__(f"{expected} expected", fname, line_num, ch_num)
 
 
+class UsingBeforeDeclarationError(ParserError):
+    def __init__(self, var_name: str, fname: str, line_num: int, ch_num: int):
+        super().__init__(f"Varible {var_name} using before declaration", fname, line_num, ch_num)
+
+
+class DoubleDeclarationError(ParserError):
+    def __init__(self, var_name: str, fname: str, line_num: int, ch_num: int):
+        super().__init__(f"Double declaration of variable {var_name}", fname, line_num, ch_num)
+
+
 class Node:
     def __init__(self, lexem):
         self._lexem = lexem
@@ -24,6 +34,9 @@ class Node:
 
     def add_child(self, node) -> None:
         self._childs.append(node)
+
+    def get_lexem(self) -> LexTableItem:
+        return self._lexem
 
 
 class Parser:
@@ -35,9 +48,10 @@ class Parser:
         self._curr_lex_index = 0
         self._block_level = 0
         self._block_id = 0
+        self._scope_stack = [(self._block_level, self._block_id)]
         self.parse()
 
-    def _go_to_next_lexeme(self) -> LexTableItem:
+    def _go_to_next_lexeme(self) -> None:
         self._curr_lex_index += 1
 
     def _is_match_cur_lexeme(self, type) -> bool:
@@ -62,16 +76,104 @@ class Parser:
         if lexeme.value != operator:
             raise ExpectedError(str(operator), self._fname, lexeme.line_num, lexeme.col_num)
 
-    def _are_lexemes_remaining(self):
+    def _expect_var_type(self, lexeme: LexTableItem, type: VariableTypes) -> None:
+        if lexeme.type != LexemTypes.IDENTIFIER:
+            raise ExpectedError(str(type) + "variable expected", self._fname, lexeme.line_num, lexeme.col_num)
+        var = self._get_variable(lexeme)
+        if var.type != type:
+            raise ExpectedError(str(type) + "variable expected", self._fname, lexeme.line_num, lexeme.col_num)
+
+    def _are_lexemes_remaining(self) -> bool:
         return self._curr_lex_index < len(self._lexemes)
 
     def _get_curr_lexeme(self) -> LexTableItem:
         return self._lexemes[self._curr_lex_index]
 
-    def _parse_string_teriminal(self):
+    def _get_variable(self, lexeme: LexTableItem) -> VariableTableItem:
+        return self._variable_table[lexeme.value]
+
+    def _enter_block(self) -> None:
+        self._block_level += 1
+        self._block_id += 1
+        self._scope_stack.append((self._block_level, self._block_id))
+
+    def _exit_block(self) -> None:
+        self._block_level -= 1
+        self._scope_stack.pop()
+
+    def _parse_block_code(self):
+        self._expect_delimiter(Delimiters.OPEN_BRACES)
+        self._enter_block()
+
+        self._exit_block()
+
+    def _parse_declare_identifier(self, var_type: VariableTypes) -> Node:
+        lexeme = self._get_curr_lexeme()
+        node = Node(lexeme)
+        curr_var = self._get_variable(lexeme)
+
+        block_level = self._scope_stack[-1][0]
+        block_id = self._scope_stack[-1][1]
+
+        if curr_var.type != VariableTypes.UNKNOWN:
+            for var in self._variable_table:
+                if curr_var.name == var.name and curr_var.block_id == var.block_id:
+                    raise DoubleDeclarationError(curr_var.name, self._fname, lexeme.line_num, lexeme.col_num)
+
+            self._variable_table.append(VariableTableItem(curr_var.name, var_type, block_level, block_id))
+            lexeme.value = len(self._variable_table) - 1
+
+        curr_var.type = var_type
+        curr_var.block_level = block_level
+        curr_var.block_id = block_id
+
+        self._go_to_next_lexeme()
+        return node
+
+    def _parse_using_identifier(self) -> Node:
+        lexeme = self._get_curr_lexeme()
+        node = Node(lexeme)
+
+        var = self._get_variable(lexeme)
+        if var.type == VariableTypes.UNKNOWN:
+            raise UsingBeforeDeclarationError(var.name, self._fname, lexeme.line_num, lexeme.col_num)
+
+        var_real_id = -1
+        for scope in reversed(self._scope_stack):
+            block_level = scope[0]
+            block_id = scope[1]
+            searched_var = VariableTableItem(var.name, var.type, block_level, block_id)
+
+            try:
+                var_real_id = self._variable_table.index(searched_var)
+                break
+            except ValueError:
+                pass
+
+        if var_real_id < 0:
+            raise UsingBeforeDeclarationError(var.name, self._fname, lexeme.line_num, lexeme.col_num)
+
+        lexeme.value = var_real_id
+        self._go_to_next_lexeme()
+        return node
+
+    def _parse_to_string(self) -> Node:
+        self._expect_key_word(KeyWords.TO_STRING)
+        self._go_to_next_lexeme()
+        self._expect_delimiter(Delimiters.OPEN_PARENTHESIS)
+        self._go_to_next_lexeme()
+        # TODO: parse arithmetic, bool or string expression.
+        self._go_to_next_lexeme()
+        self._expect_delimiter(Delimiters.CLOSE_PARENTHESIS)
+        self._go_to_next_lexeme()
+
+    def _parse_string_terminal(self) -> Node:
         lexeme = self._get_curr_lexeme()
         if lexeme.type == LexemTypes.IDENTIFIER:
-            pass
+            node = self._parse_using_identifier()
+            self._expect_var_type(node.get_lexem(), VariableTypes.STRING)
+        elif lexeme.value == KeyWords.TO_STRING:
+            node = self._parse_to_string()
 
     def _parse_string_expression(self):
         pass
@@ -99,4 +201,7 @@ class Parser:
                     self._parse_print()
                 elif is_variable_type(lexeme.value):
                     pass
+                elif lexeme.value == Delimiters.OPEN_BRACES:
+                    self._parse_block_code()
+
             self._go_to_next_lexeme()  # TODO: Delete later.
